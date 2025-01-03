b74cd8e562deb54bdcfc6d7811be141f1b104493
Shotaro Tsukada <dev@Mac-mini.local>
2025-01-03 21:16:31 +0900

    page add

--- app/src/assets/scripts/main.ts
+++ app/src/assets/scripts/main.ts
@@ -1,2 +1,12 @@
 import "../css/style.css";
 
+const openSettingsButton = document.getElementById("open-settings");
+if (openSettingsButton) {
+	openSettingsButton.addEventListener("click", () => {
+		chrome.tabs.create({ url: "pages/settings/index.html" });
+	});
+}
+
+chrome.action.onClicked.addListener(() => {
+	chrome.tabs.create({ url: "pages/settings/index.html" });
+});
